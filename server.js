36c41f0608f24ec4e861210af5803d3a5c349113
tergog <tergog@mail.ru>
2016-05-13 22:35:42 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8000;
+
+app.use(express.static(__dirname + '/public'));
+app.get('*', function(request, response) {
+	response.sendFile(__dirname + '/public/index.html');
+});
+
+
+app.listen(port,function () {
+	console.log("Listening on " + port);
+});
